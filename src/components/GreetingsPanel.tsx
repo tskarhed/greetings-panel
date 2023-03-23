7877a1188ddafd6367c5dbe5f2055364dfc26c9f
Tobias Skarhed <tobias.skarhed@gmail.com>
2023-03-23 20:55:26 +0100

    Add image fading and background

--- src/components/GreetingsPanel.tsx
+++ src/components/GreetingsPanel.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GrafanaTheme2, PanelProps } from '@grafana/data';
 import { GreetingsOptions } from 'types';
-import { css } from '@emotion/css';
+import { css, cx } from '@emotion/css';
 import { useStyles2 } from '@grafana/ui';
 
 interface Props extends PanelProps<GreetingsOptions> {}
@@ -10,7 +10,11 @@ const BASE_URL = "https://source.unsplash.com/random/";
 const PANEL_CONTENT_PADDING = 8;
 
 const getStyles = (theme: GrafanaTheme2) => {
+  
   return {
+    visibleImage: css`
+      opacity: 1;
+    `,
     textContainer: css`
       position: relative;
       z-index: 1;
@@ -29,10 +33,16 @@ const getStyles = (theme: GrafanaTheme2) => {
       margin: auto;
     `,
     image: css`
-      index: -1;
       position: absolute;
       top: 0;
       left: 0;
+      transition: opacity 1s linear;
+      opacity: 0;
+    `,
+    panelContainer: css`
+      height: 100%;
+      background-color: #8EC5FC;
+      background-image: ${theme.isDark ? 'linear-gradient(225deg, #FF3CAC 0%, #784BA0 50%, #2B86C5 100%)':'linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)'};
     `
   };
 };
@@ -44,18 +54,23 @@ export const GreetingsPanel: React.FC<Props> = ({ options, width, height }) => {
   const imageHeight = height + 2*PANEL_CONTENT_PADDING;
   const filters = encodeURIComponent(options.imageFilter.join(","));
   const url = `${BASE_URL}${imageWidth}x${imageHeight}/?${filters}`;
+
+  const [ isLoaded, setLoaded ] = useState(false);
   
 
   const selectedPhrase = options.phrases[Math.floor(Math.random() * options.phrases.length)];
   return (
-    <>
-    <div className={styles.textContainer}>
-      <p className={styles.text}>{selectedPhrase}</p>
-    </div>
+    <div className={styles.panelContainer}>
+      <div className={styles.textContainer}>
+        <p className={styles.text}>{selectedPhrase}</p>
+      </div>
       <img
       src={url}
-        className={styles.image}
+      onLoad={()=> {
+        setLoaded(true);
+      }}
+        className={cx(styles.image, {[styles.visibleImage]: isLoaded})}
       />
-    </>
+    </div>
   );
 };
